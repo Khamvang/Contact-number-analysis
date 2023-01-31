f3d8e341f4457ea4de598c98c8d9320eee3866dd
Khamvang <108178100+Khamvang@users.noreply.github.com>
2023-01-31 15:22:50 +0700

    Create script for 2023-02.sql

---
+++ script for 2023-02.sql
@@ -0,0 +1,35 @@
+#=========== Prepare contact for LCC, for Area marketing ===============
+-- 1) table contact_for  
+create table `contact_for_202211_lcc` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `file_id` int DEFAULT NULL,
+  `contact_no` varchar(255) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `province_eng` varchar(255) DEFAULT NULL,
+  `province_laos` varchar(255) DEFAULT NULL,
+  `district_eng` varchar(50) DEFAULT NULL,
+  `district_laos` varchar(255) DEFAULT NULL,
+  `village` varchar(255) DEFAULT NULL,
+  `type` varchar(255) DEFAULT NULL,
+  `maker` varchar(255) DEFAULT NULL,
+  `model` varchar(255) DEFAULT NULL,
+  `year` varchar(255) DEFAULT NULL,
+  `remark_1` varchar(255) DEFAULT null COMMENT '1=Ringi_Asset_SABC,2=Prospect_F&G that SMS success, 3=Telecom_active or SMS success, 4=ANSWERED and SMS success, 5=SMS success but NO ANSWER',
+  `remark_2` varchar(255) DEFAULT NULL,
+  `remark_3` varchar(255) DEFAULT NULL,
+  `branch_name` varchar(255) DEFAULT NULL,
+  `status` varchar(255) DEFAULT NULL,
+  `status_updated` varchar(255) DEFAULT NULL,
+  `staff_id` varchar(255) DEFAULT NULL,
+  `pvd_id` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  key `contact_no` (`contact_no`),
+  KEY `fk_file_id` (`file_id`),
+  CONSTRAINT `contact_for_202211_lcc_ibfk_1` FOREIGN KEY (`file_id`) REFERENCES `file_details` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 collate utf8mb4_general_ci ;
+
+
+
+
+
+
