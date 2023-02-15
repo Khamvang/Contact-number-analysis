2932b23d02adcb7422e98ac7c765748c692faaf7
Khamvang <108178100+Khamvang@users.noreply.github.com>
2023-02-15 14:13:24 +0700

    Update script for 2023-02.sql

--- script for 2023-02.sql
+++ script for 2023-02.sql
@@ -268,4 +268,24 @@ limit 18413
 
 
 
+-- ____________________________________________________ 4 Telecom ____________________________________________________
+-- 4) Telecom: priority 1&2 -- Campaign name: 4_Old_HO_Team2_20230201-12
+ select count(*), count(*)/3
+-- select `id`,`contact_no`,`name`,`province_eng`,`province_laos`,`district_eng`,`district_laos`,`village`,`type`,`maker`,`model`,`year`,`remark_1`,`remark_2`,`remark_3`
+from contact_for_202302_lcc cntl 
+where branch_name = 'Head Office' and `type` in( '④Telecom' ) -- '①Have Car' '②Need loan' '③Have address' '④Telecom'
+	and cntl.remark_1 in ('1', '2') -- order by cntl.remark_1;
+	and cntl.id > 0
+limit 1174
+
+-- 4) Telecom: priority 3&4 -- Campaign name: 4_Old_HO_Team2_20230201-34
+ select count(*), count(*)/15
+-- select `id`,`contact_no`,`name`,`province_eng`,`province_laos`,`district_eng`,`district_laos`,`village`,`type`,`maker`,`model`,`year`,`remark_1`,`remark_2`,`remark_3`
+from contact_for_202302_lcc cntl 
+where branch_name = 'Head Office' and `type` in( '④Telecom' ) -- '①Have Car' '②Need loan' '③Have address' '④Telecom'
+	and cntl.remark_1 in ('3', '4') -- order by cntl.remark_1;
+	and cntl.id > 0
+limit 62080
+
+
 
