519e985e4d59cda0bdc29becee5e2d481862db8f
Khamvang <108178100+Khamvang@users.noreply.github.com>
2023-02-22 10:19:30 +0700

    Update script for 2023-02.sql

--- script for 2023-02.sql
+++ script for 2023-02.sql
@@ -121,6 +121,24 @@ and ( (cntl.remark_3 = 'prospect_sabc' and cntl.status in ('F'))
 and (cntl.id in (select id from temp_sms_chairman tean where status = 1 ) -- SMS check
 or cntl.id in (select id from temp_etl_active_numbers tean2 ) ) -- ETL active
  and cntl.id not in (select id from contact_for_202302_lcc);
+ 
+ 
+ --3 to ACC
+ -- 2)
+insert into contact_for_202302_lcc
+select cntl.id, cntl.`file_id`,`contact_no`,`name`,cntl.province_eng,`province_laos`,cntl.district_eng,`district_laos`,cntl.`village`,cntl.`type`,`maker`,`model`,`year`, 
+	'1' `remark_1`,null `remark_2`,`remark_3`,cntl.`branch_name`,cntl.`status`, null `status_updated`, null `staff_id`,null `pvd_id`, date(now()) `date_provided`
+-- select count(*) -- 1199586
+from contact_numbers_to_lcc cntl
+where cntl.branch_name in ('Attapue','Bokeo','Head Office','Houaphan','LuangNamtha','Luangprabang','Oudomxay','Paksan','Pakse','Salavan','Savannakhet','Thakek','Vientiane province','Xainyabuli','Xiengkhouang'
+)
+and ( (cntl.remark_3 = 'prospect_sabc' and cntl.status in ('F')) 
+	or (cntl.remark_3 = 'prospect_sabc' and cntl.status in ('G','G1','G2') )
+	or (cntl.remark_3 = 'pbx_cdr' and cntl.status = 'NO ANSWER') )
+and (cntl.id in (select id from temp_sms_chairman tean where status = 1 ) -- SMS check
+or cntl.id in (select id from temp_etl_active_numbers tean2 ) ) -- ETL active
+and cntl.id not in (select id from contact_for_acc_jan2023_acc) 
+limit 18000;
 
 
 
