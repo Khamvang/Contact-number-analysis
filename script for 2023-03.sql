aecc7f77aa5eb7088d4195c68bc6d5f4dc6b1654
Khamvang <108178100+Khamvang@users.noreply.github.com>
2023-02-28 17:31:01 +0700

    Update script for 2023-03.sql

--- script for 2023-03.sql
+++ script for 2023-03.sql
@@ -66,39 +66,78 @@ where branch_name is null
 
 -- 1) table contact_for  
 create table `contact_for_202303_lcc` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `file_id` int DEFAULT NULL,
-  `contact_no` varchar(255) NOT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  `province_eng` varchar(255) DEFAULT NULL,
-  `province_laos` varchar(255) DEFAULT NULL,
-  `district_eng` varchar(50) DEFAULT NULL,
-  `district_laos` varchar(255) DEFAULT NULL,
-  `village` varchar(255) DEFAULT NULL,
-  `type` varchar(255) DEFAULT NULL,
-  `maker` varchar(255) DEFAULT NULL,
-  `model` varchar(255) DEFAULT NULL,
-  `year` varchar(255) DEFAULT NULL,
-  `remark_1` varchar(255) DEFAULT null COMMENT 'priority 1=Never make phone call in the past, 2=Have not call in the previous month, 3=Others',
-  `remark_2` varchar(255) DEFAULT NULL,
-  `remark_3` varchar(255) DEFAULT NULL,
-  `branch_name` varchar(255) DEFAULT NULL,
-  `status` varchar(255) DEFAULT NULL,
-  `status_updated` varchar(255) DEFAULT NULL,
-  `staff_id` varchar(255) DEFAULT NULL,
-  `pvd_id` varchar(255) DEFAULT NULL,
-  `contact_id` int(11) not null comment 'the phone number without 9020 and 9030',
-  `condition` int(11) not null comment '①『TELECOM』, ②「Need Loan」, ③「Have Car」, ④「Have Address」　＆　『GOVERNMENT』　OR  『Yellow Page』, ⑤「Have Address」　＆　others, ⑥『EXPECT』(SAB)  ＆ 　≠『EXITING』『 DORMACCY』, ⑦『EXPECT』(C)  ＆ 　≠『EXITING』『 DORMACCY』 ＆　『CAR INFORMATOIN』, ⑧have address, ⑨no address',
-  `group` int(11) not null comment '1=condition 2,3,4,6,7, 2=condition 5,8',
-  PRIMARY KEY (`id`),
-  key `contact_no` (`contact_no`),
-  key `fk_file_id` (`file_id`),
-  key `contact_id` (`contact_id`),
-  CONSTRAINT `contact_for_202303_lcc_ibfk_1` FOREIGN KEY (`file_id`) REFERENCES `file_details` (`id`)
+	  `id` int NOT NULL AUTO_INCREMENT,
+	  `file_id` int DEFAULT NULL,
+	  `contact_no` varchar(255) NOT NULL,
+	  `name` varchar(255) DEFAULT NULL,
+	  `province_eng` varchar(255) DEFAULT NULL,
+	  `province_laos` varchar(255) DEFAULT NULL,
+	  `district_eng` varchar(50) DEFAULT NULL,
+	  `district_laos` varchar(255) DEFAULT NULL,
+	  `village` varchar(255) DEFAULT NULL,
+	  `type` varchar(255) DEFAULT NULL,
+	  `maker` varchar(255) DEFAULT NULL,
+	  `model` varchar(255) DEFAULT NULL,
+	  `year` varchar(255) DEFAULT NULL,
+	  `remark_1` varchar(255) DEFAULT null COMMENT 'priority 1=Never make phone call in the past, 2=Have not call in the previous month, 3=Others',
+	  `remark_2` varchar(255) DEFAULT NULL,
+	  `remark_3` varchar(255) DEFAULT NULL,
+	  `branch_name` varchar(255) DEFAULT NULL,
+	  `status` varchar(255) DEFAULT NULL,
+	  `status_updated` varchar(255) DEFAULT NULL,
+	  `staff_id` varchar(255) DEFAULT NULL,
+	  `pvd_id` varchar(255) DEFAULT NULL,
+	  `contact_id` int(11) not null comment 'the phone number without 9020 and 9030',
+	  `condition` int(11) not null comment '①『TELECOM』, ②「Need Loan」, ③「Have Car」, ④「Have Address」　＆　『GOVERNMENT』　OR  『Yellow Page』, ⑤「Have Address」　＆　others, ⑥『EXPECT』(SAB)  ＆ 　≠『EXITING』『 DORMACCY』, ⑦『EXPECT』(C)  ＆ 　≠『EXITING』『 DORMACCY』 ＆　『CAR INFORMATOIN』, ⑧have address, ⑨no address',
+	  `group` int(11) not null comment '1=condition 2,3,4,6,7, 2=condition 5,8',
+	  PRIMARY KEY (`id`),
+	  key `contact_no` (`contact_no`),
+	  key `fk_file_id` (`file_id`),
+	  key `contact_id` (`contact_id`),
+	  CONSTRAINT `contact_for_202303_lcc_ibfk_1` FOREIGN KEY (`file_id`) REFERENCES `file_details` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 collate utf8mb4_general_ci ;
 
 
 
+# ______________________________________________________ insert into contact_for_202303_lcc ______________________________________________________________ #
+-- 1)
+insert into contact_for_202303_lcc
+select cntl.id, cntl.`file_id`,`contact_no`,`name`,cntl.province_eng,`province_laos`,cntl.district_eng,`district_laos`,cntl.`village`,cntl.`type`,`maker`,`model`,`year`, 
+	case when cntl.status is null then 1
+		when TIMESTAMPDIFF(MONTH, cntl.`date_updated`, date(now())) = 0 then 3 -- current month
+		when TIMESTAMPDIFF(MONTH, cntl.`date_updated`, date(now())) = 1 then 3 -- before current month
+		else 2
+	end`remark_1`,
+	null `remark_2`,`remark_3`,cntl.`branch_name`,cntl.`status`, null `status_updated`, null `staff_id`,null `pvd_id`, 
+	case when left(cntl.contact_no,4) = '9020' then right(cntl.contact_no,8) when left(cntl.contact_no,4) = '9030' then right(cntl.contact_no,7) end `contact_id`, 
+	case when cntl.`type` = '①Have Car' then 3
+		when cntl.`type` = '②Need loan' then 2
+		when cntl.`type` = '③Have address' and fd.category = '①GOVERNMENT' then 4
+		when cntl.`type` = '③Have address' and fd.category != '①GOVERNMENT' then 5
+		when cntl.`type` = '④Telecom' then 1
+	end `condition`, 
+	case when cntl.`type` in ('①Have Car', '②Need loan') then 1
+		when cntl.`type` = '③Have address' and fd.category = '①GOVERNMENT' then 1
+		when cntl.`type` = '③Have address' and fd.category != '①GOVERNMENT' then 2
+		when cntl.`type` = '④Telecom' then 4
+	end `group`
+-- select count(*) --  4,278,841
+from contact_numbers_to_lcc cntl left join file_details fd on (fd.id = cntl.file_id)
+where (cntl.remark_3 = 'contracted' or cntl.remark_3 = 'ringi_not_contract' or cntl.remark_3 = 'aseet_not_contract' 
+	or (cntl.remark_3 = 'prospect_sabc' and cntl.status in ('S','A','B','C'))
+	or (cntl.remark_3 = 'pbx_cdr' and cntl.status = 'ANSWERED')
+	or (cntl.remark_3 = 'Telecom' and cntl.status = 'ETL_active')
+	or (cntl.remark_3 = 'Telecom' and cntl.status = 'SMS_success') ) -- 1
+	or (
+		( ( cntl.remark_3 = 'prospect_sabc' and cntl.status in ('F')) 
+		or (cntl.remark_3 = 'prospect_sabc' and cntl.status in ('G','G1','G2') )
+		or (cntl.remark_3 = 'pbx_cdr' and cntl.status = 'NO ANSWER') )
+		and (cntl.id in (select id from temp_sms_chairman tean where status = 1 ) -- SMS check
+		or cntl.id in (select id from temp_etl_active_numbers tean2 ) ) 
+		) -- 2
+	or cntl.status is null -- new number
+
+ 
 
 
 
