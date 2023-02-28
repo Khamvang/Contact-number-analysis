08bc1f5b6aacd4d0c03fa4dc346086ed13fe6366
Khamvang <108178100+Khamvang@users.noreply.github.com>
2023-02-28 16:38:58 +0700

    Update script for 2023-03.sql

--- script for 2023-03.sql
+++ script for 2023-03.sql
@@ -42,6 +42,8 @@ select * , count(*) from
 	) t
 group by branch_name , province_eng , `type` , category , category2 , `address`, `car_info`, `result` ;
 
+
+
 -- ____________________________________ Check and update the branch_name is null ____________________________________ --
 update contact_numbers_to_lcc cntl set branch_name = 'Bokeo'
 where branch_name is null
@@ -59,4 +61,49 @@ where branch_name is null
 			) -- 2
 		or cntl.status is null -- new number
 		)
+
+
+
+-- 1) table contact_for  
+create table `contact_for_202303_lcc` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `file_id` int DEFAULT NULL,
+  `contact_no` varchar(255) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `province_eng` varchar(255) DEFAULT NULL,
+  `province_laos` varchar(255) DEFAULT NULL,
+  `district_eng` varchar(50) DEFAULT NULL,
+  `district_laos` varchar(255) DEFAULT NULL,
+  `village` varchar(255) DEFAULT NULL,
+  `type` varchar(255) DEFAULT NULL,
+  `maker` varchar(255) DEFAULT NULL,
+  `model` varchar(255) DEFAULT NULL,
+  `year` varchar(255) DEFAULT NULL,
+  `remark_1` varchar(255) DEFAULT null COMMENT 'priority 1=Never make phone call in the past, 2=Have not call in the previous month, 3=Others',
+  `remark_2` varchar(255) DEFAULT NULL,
+  `remark_3` varchar(255) DEFAULT NULL,
+  `branch_name` varchar(255) DEFAULT NULL,
+  `status` varchar(255) DEFAULT NULL,
+  `status_updated` varchar(255) DEFAULT NULL,
+  `staff_id` varchar(255) DEFAULT NULL,
+  `pvd_id` varchar(255) DEFAULT NULL,
+  `contact_id` int(11) not null comment 'the phone number without 9020 and 9030',
+  `condition` int(11) not null comment '①『TELECOM』, ②「Need Loan」, ③「Have Car」, ④「Have Address」　＆　『GOVERNMENT』　OR  『Yellow Page』, ⑤「Have Address」　＆　others, ⑥『EXPECT』(SAB)  ＆ 　≠『EXITING』『 DORMACCY』, ⑦『EXPECT』(C)  ＆ 　≠『EXITING』『 DORMACCY』 ＆　『CAR INFORMATOIN』, ⑧have address, ⑨no address',
+  `group` int(11) not null comment '1=condition 2,3,4,6,7, 2=condition 5,8',
+  PRIMARY KEY (`id`),
+  key `contact_no` (`contact_no`),
+  key `fk_file_id` (`file_id`),
+  key `contact_id` (`contact_id`),
+  CONSTRAINT `contact_for_202303_lcc_ibfk_1` FOREIGN KEY (`file_id`) REFERENCES `file_details` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 collate utf8mb4_general_ci ;
+
+
+
+
+
+
+
+
+
+
 
