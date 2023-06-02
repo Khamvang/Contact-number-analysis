765cbcb29e5f4698411f0be1bdc49a202fabbfd5
Khamvang <108178100+Khamvang@users.noreply.github.com>
2023-06-02 16:18:55 +0700

    Create script for 2023-06.sql

---
+++ script for 2023-06.sql
@@ -0,0 +1,99 @@
+
+
+-- 1) table contact_for  
+create table `contact_for_202306_lcc` (
+	  `id` int NOT NULL AUTO_INCREMENT,
+	  `file_id` int DEFAULT NULL,
+	  `contact_no` varchar(255) NOT NULL,
+	  `name` varchar(255) DEFAULT NULL,
+	  `province_eng` varchar(255) DEFAULT NULL,
+	  `province_laos` varchar(255) DEFAULT NULL,
+	  `district_eng` varchar(50) DEFAULT NULL,
+	  `district_laos` varchar(255) DEFAULT NULL,
+	  `village` varchar(255) DEFAULT NULL,
+	  `type` varchar(255) DEFAULT NULL,
+	  `maker` varchar(255) DEFAULT NULL,
+	  `model` varchar(255) DEFAULT NULL,
+	  `year` varchar(255) DEFAULT NULL,
+	  `remark_1` varchar(255) DEFAULT null COMMENT 'priority 1=P1: New number, 2=P2: ③Have address, 3=P3: ②Need loan, 4=P4: ①Have Car, 5=P5: ④Telecom',
+	  `remark_2` varchar(255) DEFAULT NULL,
+	  `remark_3` varchar(255) DEFAULT NULL,
+	  `branch_name` varchar(255) DEFAULT NULL,
+	  `status` varchar(255) DEFAULT NULL,
+	  `status_updated` varchar(255) DEFAULT NULL,
+	  `staff_id` varchar(255) DEFAULT NULL,
+	  `pvd_id` varchar(255) DEFAULT NULL,
+	  `contact_id` int(11) not null comment 'the phone number without 9020 and 9030',
+	  `condition` int(11) not null comment '①「Need Loan」, ②「Have Car」, ③「Have Address」　＆　『GOVERNMENT』　OR  『Yellow Page』, ④「Have Address」　＆　others, ⑤『EXPECT』(SAB)  ＆ 　≠『EXITING』『 DORMACCY』, ⑥『EXPECT』(C)  ＆ 　≠『EXITING』『 DORMACCY』 ＆　『CAR INFORMATOIN』, ⑦『TELECOM』have address, ⑧『TELECOM』no address',
+	  `group` int(11) not null comment '1=condition 1,2,3,5,6, 2=condition 4,7, 3=condition 8',
+	  PRIMARY KEY (`id`),
+	  key `contact_no` (`contact_no`),
+	  key `fk_file_id` (`file_id`),
+	  key `contact_id` (`contact_id`),
+	  CONSTRAINT `contact_for_202306_lcc_ibfk_1` FOREIGN KEY (`file_id`) REFERENCES `file_details` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 collate utf8mb4_general_ci ;
+
+
+
+-- 2) intert data from contact_numbers_to_lcc to table monthly
+insert into contact_for_202306_lcc
+ select cntl.id, cntl.`file_id`,`contact_no`,`name`,cntl.province_eng,`province_laos`,cntl.district_eng,`district_laos`,cntl.`village`,cntl.`type`,`maker`,`model`,`year`, 
+	case when cntl.status = '' then '1'
+		when cntl.`type` = '①Have Car' then '2'
+		when cntl.`type` = '②Need loan' then '3'
+		when cntl.`type` = '③Have address' then '4'
+		when cntl.`type` = 'prospect' then '5'
+		when cntl.`type` = '④Telecom' then '6'
+	end `remark_1`,
+	null `remark_2`,`remark_3`,cntl.`branch_name`,cntl.`status`, null `status_updated`, null `staff_id`,null `pvd_id`, 
+	case when left(cntl.contact_no,4) = '9020' then right(cntl.contact_no,8) when left(cntl.contact_no,4) = '9030' then right(cntl.contact_no,7) end `contact_id`, 
+	case when cntl.`type` = '①Have Car' then 2
+		when cntl.`type` = '②Need loan' then 1
+		when cntl.`type` = '③Have address' and fd.category = '①GOVERNMENT' then 3
+		when cntl.`type` = '③Have address' and fd.category != '①GOVERNMENT' then 4
+		when cntl.`type` = '④Telecom' and (cntl.province_eng is not null and cntl.district_eng is not null and cntl.village is not null ) then 7
+		when cntl.`type` = '④Telecom' then 8
+	end `condition`, 
+	case when cntl.`type` in ('①Have Car', '②Need loan') then 1
+		when cntl.`type` = '③Have address' and fd.category = '①GOVERNMENT' then 1
+		when cntl.`type` = '③Have address' and fd.category != '①GOVERNMENT' then 2
+		when cntl.`type` = '④Telecom' and (cntl.province_eng is not null and cntl.district_eng is not null and cntl.village is not null ) then 2
+		when cntl.`type` = '④Telecom' then 3
+	end `group`
+-- select count(*) -- 
+from contact_numbers_to_lcc cntl left join file_details fd on (fd.id = cntl.file_id)
+where (cntl.remark_3 in ('contracted', 'ringi_not_contract', 'aseet_not_contract') ) -- already register on LMS
+	or (cntl.remark_3 in ('prospect_sabc', 'lcc') and cntl.status in ('X','S','A','B','C', 'FF1 not_answer', 'FF2 power_off') ) -- already register on CRM and LCC
+	or (cntl.remark_3 = 'pbx_cdr' and cntl.status = 'ANSWERED') -- Ever Answered in the past
+	or (cntl.remark_3 = 'Telecom' and cntl.status in ('ETL_active', 'SMS_success') ) -- Ever sent SMS and success
+	or cntl.status is null -- new number
+	or cntl.contact_id in (select contact_id from temp_sms_chairman tean where status = 1 ) -- SMS check
+	or cntl.contact_id in (select contact_id from temp_etl_active_numbers tean2 ) -- ETL active 
+
+
+
+-- 3) delete the status contracted, inactive number from table monthly
+select * from contact_for_202306_lcc cntl 
+-- delete from contact_for_202306_lcc cntl
+where cntl.remark_3 = 'contracted' -- contracted
+	or (cntl.remark_3 = 'lcc' and cntl.status = 'Block need_to_block') -- Block need_to_block
+	or (cntl.remark_3 = 'lcc' and cntl.status in ('FFF can_not_contact', 'No have in telecom')) -- FFF can_not_contact
+	or (cntl.remark_3 = 'Telecom' and cntl.status in ('ETL_inactive','SMS_Failed')) -- Telecom_inactive
+	or cntl.remark_3 = 'blacklist' -- blacklist
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
