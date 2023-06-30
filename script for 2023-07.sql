6d8edf67fffd8d3324b7ee9c4e6d34c34f1e1fdf
Khamvang <108178100+Khamvang@users.noreply.github.com>
2023-06-30 09:43:08 +0700

    Update script for 2023-07.sql

--- script for 2023-07.sql
+++ script for 2023-07.sql
@@ -85,6 +85,81 @@ where remark_3 = 'contracted' -- contracted
 
 
 
+-- Export from lalcodb to contact_for_lcc_prospectsabc for priority 5
+select * , case when left(contact_no, 4) = '9020' then right(contact_no, 8) when left(contact_no, 4) = '9030' then right(contact_no, 7) end "contact_id",
+	case when status in ('S','A','B') then '5' when status = 'C' and (maker !='' or model != '') then 6 else 6 end "condition",
+	'1' "group"
+from (
+	select null "id",
+		case when left(right (translate (tel, translate(tel, '0123456789', ''), ''), 8), 1)= '0' then concat('903', right (translate (tel, translate(tel, '0123456789', ''), ''), 8))
+			when length( translate (tel, translate(tel, '0123456789', ''), '')) = 7 then concat('9030', right (translate (tel, translate(tel, '0123456789', ''), ''), 8))
+			else concat('9020', right (translate (tel, translate(tel, '0123456789', ''), ''), 8))
+		end "contact_no",
+		translate (concat(firstname, ' ', lastname), '.,:,', '') "name",
+		case when province = 'Attapeu' then 'ATTAPUE'
+			when province = 'Bokeo' then 'BORKEO'
+			when province = 'Bolikhamxai' then 'BORLIKHAMXAY'
+			when province = 'Champasak' then 'CHAMPASACK'
+			when province = 'Houaphan' then 'HUAPHAN'
+			when province = 'Khammouan' then 'KHAMMOUAN'
+			when province = 'Louangphrabang' then 'LUANG PRABANG'
+			when province = 'Louang Namtha' then 'LUANGNAMTHA'
+			when province = 'Oudomxai' then 'OUDOMXAY'
+			when province = 'Phongsali' then 'PHONGSALY'
+			when province = 'Saravan' then 'SALAVANH'
+			when province = 'Savannakhet' then 'SAVANNAKHET'
+			when province = 'Vientiane Cap' then 'VIENTIANE CAPITAL'
+			when province = 'Vientiane province' then 'VIENTIANE PROVINCE'
+			when province = 'Xaignabouri' then 'XAYABOULY'
+			when province = 'Xaisomboun' then 'XAYSOMBOUN'
+			when province = 'Xekong' then 'XEKONG'
+			when province = 'Xiangkhoang' then 'XIENGKHUANG'
+			else null
+		end "province_eng",
+		null "province_laos",
+		translate (district, '.,:,', '') "district_eng",
+		null "district_laos",
+		translate (c.addr , '.,:,`', '') "village",
+		'prospect' "type",
+		translate (maker, '.,:,`', '') "maker",
+		translate (model, '.,:,`', '') "model",
+		"year",
+		5 "remark_1",
+		null "remark_2",
+		'prospect_sabc' "remark_3",
+		null "branch_name",
+		rank1 "status",
+		null "staff_id",
+		tua.new_village_id "pvd_id"
+	from custtbl c left join temp_update_address tua on (c.id = tua.id)
+	where c.rank1 in ('S','A','B') or (c.rank1 = 'C' and (c.maker !='' or c.model != '') )
+	order by inputdate desc ) t
+where concat(length("contact_no"), left( "contact_no", 5)) in ('1190302','1190304','1190305','1190307','1190309','1290202','1290205','1290207','1290209');
+
+
+
+-- check and delete douplicate number 
+
+-- 6)delete duplicate and check data
+delete from removed_duplicate_2;
+select count(*) from contact_for_lcc_prospectsabc; -- 38584 >> 
+insert into removed_duplicate_2 
+select id, row_numbers, now() `time` from ( 
+		select id , row_number() over (partition by contact_no order by field(`type`, "contracted", "ringi_not_contract", "aseet_not_contract",
+			"prospect_sabc", "pbx_cdr", "lcc") ,
+		FIELD(`status` , "Active", "Closed", "Refinance", "Disbursement Approval", "Pending Disbursement", "Pending Approval", "Pending",
+		"Approved", "Pending Approval from Credit", "Asset Assessed", "Pending Assessment", "Draft", "Cancelled", "Deleted",
+		"X", "S", "A", "B", "C", "F", "G", "G1", "G2", "ANSWERED", "NO ANSWER", "Block need_to_block", "FF1 not_answer", "FF2 power_off", "FFF can_not_contact")) as row_numbers  
+		from contact_for_lcc_prospectsabc  
+		) as t1
+	where row_numbers > 1;
+
+delete from contact_for_lcc_prospectsabc where id in (select id from removed_duplicate_2 );
+
+select * from contact_for_lcc_prospectsabc where contact_id in (select contact_id from contact_for_202307_lcc);
+
+delete from contact_for_lcc_prospectsabc where contact_id in (select contact_id from contact_for_202307_lcc);
+
 
 
 
