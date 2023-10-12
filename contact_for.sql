574f380711644e4ba8b56581aa84e28bf23c860a
Khamvang <108178100+Khamvang@users.noreply.github.com>
2023-10-12 18:16:51 +0700

    Create contact_for.sql

---
+++ contact_for.sql
@@ -0,0 +1,62 @@
+
+
+-- 1) table contact_for  
+create table `contact_for_logcall` (
+	  `date` date not null ,
+	  `contact_no` varchar(255) not null,
+	  `contact_id` int(11) not null comment 'the phone number without 9020 and 9030',
+	  `remark_2` varchar(255) default null,
+	  `status_updated` varchar(255) default null,
+	  key `contact_id` (`contact_id`)
+) engine=InnoDB auto_increment=1 default CHARSET=utf8mb4 collate utf8mb4_general_ci ;
+
+-- insert data from each month into table contact_for_logcall
+-- Jan 2023
+insert into contact_for_logcall
+select '2023-01-31' `date`, `contact_no`, `contact_id`, `remark_2`, `status_updated`
+from contact_for_202301_lcc cfl where status_updated is not null;
+
+-- Feb 2023
+insert into contact_for_logcall
+select '2023-02-28' `date`, `contact_no`, `contact_id`, `remark_2`, `status_updated`
+from contact_for_202302_lcc cfl where status_updated is not null;
+
+-- Mar 2023
+insert into contact_for_logcall
+select '2023-03-31' `date`, `contact_no`, `contact_id`, `remark_2`, `status_updated`
+from contact_for_202303_lcc cfl where status_updated is not null;
+
+-- Apr 2023
+insert into contact_for_logcall
+select '2023-04-30' `date`, `contact_no`, `contact_id`, `remark_2`, `status_updated`
+from contact_for_202304_lcc cfl where status_updated is not null;
+
+-- May 2023
+insert into contact_for_logcall
+select '2023-05-31' `date`, `contact_no`, `contact_id`, `remark_2`, `status_updated`
+from contact_for_202305_lcc cfl where status_updated is not null;
+
+-- Jun 2023
+insert into contact_for_logcall
+select '2023-06-30' `date`, `contact_no`, `contact_id`, `remark_2`, `status_updated`
+from contact_for_202306_lcc cfl where status_updated is not null;
+
+-- Jul 2023
+insert into contact_for_logcall
+select '2023-07-31' `date`, `contact_no`, `contact_id`, `remark_2`, `status_updated`
+from contact_for_202307_lcc cfl where status_updated is not null;
+
+-- Aug 2023
+insert into contact_for_logcall
+select '2023-08-31' `date`, `contact_no`, `contact_id`, `remark_2`, `status_updated`
+from contact_for_202308_lcc cfl where status_updated is not null;
+
+-- Sep 2023
+insert into contact_for_logcall
+select '2023-09-30' `date`, `contact_no`, `contact_id`, `remark_2`, `status_updated`
+from contact_for_202309_lcc cfl where status_updated is not null;
+
+
+
+
+
