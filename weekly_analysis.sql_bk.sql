d853b2a047ee49e02324d7216f3847356f564e65
Khamvang <108178100+Khamvang@users.noreply.github.com>
2023-10-30 19:08:31 +0700

    Update weekly_analysis.sql_bk.sql

--- weekly_analysis.sql_bk.sql
+++ weekly_analysis.sql_bk.sql
@@ -618,12 +618,3 @@ alter table contact_numbers_to_lcc add `contact_id` int(11) not null;
 alter table contact_numbers_to_lcc add key `contact_id` (`contact_id`);
 
 update contact_numbers_to_lcc set contact_id = case when left(contact_no,4) = '9020' then right(contact_no,8) when left(contact_no,4) = '9030' then right(contact_no,7) end ;
-
-
-
-
-
-
-
-
-
