2e8b1075c5cacceae8af337932a145a52adbcc9e
Khamvang <108178100+Khamvang@users.noreply.github.com>
2023-12-15 16:05:47 +0700

    Update script for 2023-12.sql

--- script for 2023-12.sql
+++ script for 2023-12.sql
@@ -102,6 +102,91 @@ select count(*) from contact_for_202312_lcc cfl --
  -- > this month not need because we import to frappe table: tabSME_BO_and_Plan  and make them use from there
 
 
+-- ______________________________________________________ check and update logcall ______________________________________________________
+select cfl.contact_id, t.`count_time` from contact_for_202312_lcc cfl
+left join (select contact_id, count(*) `count_time` from contact_for_logcall group by contact_id) t on (cfl.contact_id = t.contact_id) ;
+
+update contact_for_202312_lcc cfl
+left join (select contact_id, count(*) `count_time` from contact_for_logcall group by contact_id) t on (cfl.contact_id = t.contact_id)
+set cfl.`condition` = t.`count_time` ;
+
+-- update the condition is null
+select * from contact_for_202312_lcc where `condition` is null; -- 1,356,166
+update contact_for_202312_lcc set `condition` = 1 where `condition` is null and status is not null; -- 1,335,744
+update contact_for_202312_lcc set `condition` = 1 where `condition` is null and status is null; -- 422
+
+
+
+
+# ______________________________________________________ export to create campaign on LCC for contact_for_202312_lcc ______________________________________________________________ #
+
+Branch: 'Attapue','Bokeo','Head Office','Houaphan','LuangNamtha','Luangprabang','Oudomxay','Paksan','Pakse','Salavan','Savannakhet','Thakek','Vientiane province','Xainyabuli','Xiengkhouang'
+Team: 'ATP Team', 'Bokeo', 'Team2', 'Team3', 'Team4', 'Houaphan Team', 'Luangnamtha', 'LPB Team', 'OUX Team', 'Paksan Team', 'Pakse Team', 'Salavan', 'SVK Team', 'Thakket Team', 'VTP Team', 'XYB Team', 'XKH Team'
+
+-- ____________________________________________________ Priority1 ____________________________________________________
+-- Campaign name: all_new_ATTAPUE_ATP Team_20231201_p1
+-- select count(*) -- 
+select `id`,`contact_no`,`name`,`province_eng`,`province_laos`,`district_eng`,`district_laos`,`village`,`type`,`maker`,`model`,`year`,`remark_1`,`remark_2`,`remark_3`
+from contact_for_202312_lcc cntl 
+where cntl.remark_1 in ('1') 
+	-- and branch_name = 'Attapue' ; -- not need add branch 
+
+
+-- ____________________________________________________ Priority2 ____________________________________________________
+-- Campaign name: 3_Old_ATTAPUE_ATP Team_20231201_p2
+-- select count(*) -- 
+select `id`,`contact_no`,`name`,`province_eng`,`province_laos`,`district_eng`,`district_laos`,`village`,`type`,`maker`,`model`,`year`,`remark_1`,`remark_2`,`remark_3`
+from contact_for_202312_lcc cntl 
+where cntl.remark_1 in ('2')
+	and branch_name = 'Attapue' ;
+
+
+-- ____________________________________________________ Priority3 ____________________________________________________
+-- Campaign name: 2_Old_ATTAPUE_ATP Team_20231201_p3
+-- select count(*) -- 
+select `id`,`contact_no`,`name`,`province_eng`,`province_laos`,`district_eng`,`district_laos`,`village`,`type`,`maker`,`model`,`year`,`remark_1`,`remark_2`,`remark_3`
+from contact_for_202312_lcc cntl 
+where cntl.remark_1 in ('3') and cntl.`condition` <= 1
+	and branch_name = 'Attapue';
+
+
+-- ____________________________________________________ Priority4 ____________________________________________________
+-- Campaign name: 1_Old_ATTAPUE_ATP Team_20231201_p4
+-- select count(*) -- 
+select `id`,`contact_no`,`name`,`province_eng`,`province_laos`,`district_eng`,`district_laos`,`village`,`type`,`maker`,`model`,`year`,`remark_1`,`remark_2`,`remark_3`
+from contact_for_202312_lcc cntl 
+where cntl.remark_1 in ('4') and cntl.`condition` <= 1
+	and branch_name = 'Attapue' ;
+
+
+-- ____________________________________________________ Priority5 ____________________________________________________
+-- Campaign name: p_Old_ATTAPUE_ATP Team_20231201_p5
+-- select count(*) -- 
+select `id`,`contact_no`,`name`,`province_eng`,`province_laos`,`district_eng`,`district_laos`,`village`,`type`,`maker`,`model`,`year`,`remark_1`,`remark_2`,`remark_3`
+from contact_for_202312_lcc cntl 
+where cntl.remark_1 in ('5')
+	and branch_name = 'Attapue' ;
+
+
+-- ____________________________________________________ Priority6 ____________________________________________________
+-- Campaign name: 4_Old_ATTAPUE_ATP Team_20231201_p6
+-- select count(*) -- 
+select `id`,`contact_no`,`name`,`province_eng`,`province_laos`,`district_eng`,`district_laos`,`village`,`type`,`maker`,`model`,`year`,`remark_1`,`remark_2`,`remark_3`
+from contact_for_202312_lcc cntl 
+where cntl.remark_1 in ('6') and cntl.`condition` = 0
+	and branch_name = 'Attapue' ;
+
+
+-- ____________________________________________________ Priority6 ____________________________________________________
+-- Campaign name: 4_Old_Head Office_Team2_20231201_p6
+-- select count(*) -- 1125547
+select `id`,`contact_no`,`name`,`province_eng`,`province_laos`,`district_eng`,`district_laos`,`village`,`type`,`maker`,`model`,`year`,`remark_1`,`remark_2`,`remark_3`
+from contact_for_202312_lcc cntl 
+where cntl.remark_1 in ('6') and cntl.`condition` = 0
+	 and branch_name = 'Head Office' -- limit 0, 3 -- mean start from row 0+1 and of row 0+3
+	-- limit 0*75100 , 75100 -- result will be start from 0*75100+1, end 0*75100+75100, limit n+1, n (start from n+1, end of n)
+	-- limit 1*75100, 75100 -- result will be start from 1*75100+1, end at 1*75100+75100
+	 -- limit 2*75100, 75100 -- result will be start from 2*75100+1, end at 2*75100+75100
 
 
 
