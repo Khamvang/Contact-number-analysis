fad93a6ac88b66a9be1033a56b82051f8acce556
Khamvang <108178100+Khamvang@users.noreply.github.com>
2024-08-01 08:05:10 +0700

    Update IVR_campaign.sql

--- IVR_campaign.sql
+++ IVR_campaign.sql
@@ -34,5 +34,27 @@ join customers on campaign_calls.customer_id=customers.id
 where campaign_calls.campaign_id=4189
 order by campaigncall_id, flag, info;
 
+======================================================================================================================================================
+
+ -- prepare list for new 13 branches
+select `id`,`contact_no`,`name`,`province_eng`,`province_laos`,`district_eng`,`district_laos`,`village`,`type`,`maker`,`model`,`year`,`remark_1`,`condition` `remark_2`,`remark_3`
+from contact_for_202406_lcc cntl 
+where concat(province_eng, ' - ', district_eng) in ('Attapeu - Saysetha','Borikhamxay - Khamkeut','Champasack - Paksong','Champasack - Phonthong','Luangprabang - Nam Bak','Savanakhet - Songkhone','Vientiane Capital - Naxaythong','Vientiane Capital - Xaythany','Vientiane Capital - Hadxayfong','Vientiane Capital - Parkngum','Vientiane Province - Vangvieng','Xayaboury - Parklai','Xiengkhuang - Kham'
+)
+order by branch_name, province_eng, district_eng;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
